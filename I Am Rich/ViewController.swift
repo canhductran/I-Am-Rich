49ec6cf668d331c5cfc595a908ed5bbea320dd47
Canh Duc Tran <CHRIS@Canhs-MacBook-Pro.local>
2019-08-18 13:37:54 +1000

    Implemented I Am Rich app

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Canh Duc Tran on 20/7/19.
+//  Copyright © 2019 Canh Duc Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
